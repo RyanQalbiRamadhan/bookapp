dc8213215a9850ff7dfb867621c81c123117bc9b
RyanQalbiRamadhan <ryanqalbiramadhan@gmail.com>
2020-10-28 21:19:46 +0700

    Create model, migration, and controller for book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,15 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
 class Book extends Model
 {
-  use HasFactory;
   protected $fillable = ['title', 'description', 'author'];
 }
