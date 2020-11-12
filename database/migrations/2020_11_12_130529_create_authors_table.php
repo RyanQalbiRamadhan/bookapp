26842955dc2d89e43338e583802a482bddef7cf6
RyanQalbiRamadhan <ryanqalbiramadhan@gmail.com>
2020-11-12 20:59:03 +0700

    Create migration for author

--- database/migrations/2020_11_12_130529_create_authors_table.php
+++ database/migrations/2020_11_12_130529_create_authors_table.php
@@ -16,7 +16,7 @@ class CreateAuthorsTable extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('gender', ['mal', 'female']);
+            $table->enum('gender', ['male', 'female']);
             $table->text('biography');
             $table->timestamps();
         });
